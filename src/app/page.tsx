9f5cf58d4ec7d37f4f1134b9f01acc540de95250
Coochiee <sugarytchannel@gmail.com>
2024-12-27 01:49:56 +0530

    cards fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,31 @@ import { WorldMapDemo } from "./Features/WorldMapDemo";
 import { CardDemo } from "./Features/Cards";
 
 export default function Home() {
+
+  const cardsData = [
+    {
+      title: "Unmatched Speed",
+      description:
+        "Proprietary quantum chips with unmatched processing speed.",
+      imageUrl:"/1.webp",
+    },
+    {
+      title: "For the Masses",
+      description: "Turnkey quantum systems for businesses and research institutions.",
+      imageUrl:"/2.webp", 
+    },
+    {
+      title: "Industry Solutions",
+      description: "Custom solutions for industries like finance, healthcare, and AI.",
+      imageUrl: "/3.webp",
+    },
+    {
+      title: "Empowering Innovation",
+      description: "Supportive infrastructure for quantum developers and researchers",
+      imageUrl: "/4.webp",
+    },
+  ];
+
   return (
     <div className="h-auto w-screen md:w-auto overflow-x-hidden bg-black flex flex-col items-center">
       <div className="my-5 text-white">
@@ -28,12 +53,18 @@ export default function Home() {
       <div className="my-2">
         <WorldMapDemo />
       </div>
-      <div>
-        <CardDemo/>
-        <CardDemo/>
-        <CardDemo/>
-        <CardDemo/>
+      <div className="container mx-auto px-4 py-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {cardsData.map((card) => (
+          <CardDemo 
+            key={card.title} 
+            title={card.title} 
+            description={card.description} 
+            imageUrl={card.imageUrl} 
+          />
+        ))}
       </div>
+    </div>
     </div>
   );
 }
